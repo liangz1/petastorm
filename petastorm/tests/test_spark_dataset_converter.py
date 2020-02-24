aeea8f209ed3a15add4c43d19efaec6f115e0d32
Liang Zhang <liang.zhang@databricks.com>
2020-02-23 22:15:20 -0800

    [ML-9890] Add tests for all primitive types

--- petastorm/tests/test_spark_dataset_converter.py
+++ petastorm/tests/test_spark_dataset_converter.py
@@ -1,7 +1,7 @@
 from petastorm.spark.spark_dataset_converter import make_spark_converter, SparkDatasetConverter
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, \
-    BooleanType, FloatType, ShortType, IntegerType, LongType, DoubleType
+    BooleanType, FloatType, ShortType, IntegerType, LongType, DoubleType, StringType, BinaryType, ByteType
 
 import numpy as np
 import os
@@ -24,11 +24,15 @@ class TfConverterTest(unittest.TestCase):
             StructField("double_col", DoubleType(), False),
             StructField("short_col", ShortType(), False),
             StructField("int_col", IntegerType(), False),
-            StructField("long_col", LongType(), False)
+            StructField("long_col", LongType(), False),
+            StructField("str_col", StringType(), False),
+            StructField("bin_col", BinaryType(), False),
+            StructField("byte_col", ByteType(), False),
         ])
         df = self.spark.createDataFrame([
-            (True, 0.12, 432.1, 5, 5, 0),
-            (False, 123.45, 0.987, 9, 908, 765)], schema=schema).coalesce(1)
+            (True, 0.12, 432.1, 5, 5, 0, "hello", b"spark", -128),
+            (False, 123.45, 0.987, 9, 908, 765, "petastorm", b"12345", 127)], schema=schema)\
+            .coalesce(1)
         # If we use numPartition > 1, the order of the loaded dataset would be non-deterministic.
         expected_df = df.collect()
 
@@ -42,16 +46,24 @@ class TfConverterTest(unittest.TestCase):
                 # Now we only have one batch.
             for i in range(converter.dataset_size):
                 for col in df.schema.names:
-                    self.assertEquals(getattr(ts, col)[i], expected_df[i][col])
+                    actual_ele = getattr(ts, col)[i]
+                    expected_ele = expected_df[i][col]
+                    if type(actual_ele) == bytes:
+                        actual_ele = actual_ele.decode()
+                    if type(expected_ele) == bytearray:
+                        expected_ele = expected_ele.decode()
+                    self.assertEqual(actual_ele, expected_ele)
 
-            self.assertEquals(len(converter), len(expected_df))
+            self.assertEqual(len(converter), len(expected_df))
 
-        self.assertEquals(ts.bool_col.dtype.type, np.bool_, "Boolean type column is not inferred correctly.")
-        self.assertEquals(ts.float_col.dtype.type, np.float32, "Float type column is not inferred correctly.")
-        self.assertEquals(ts.double_col.dtype.type, np.float64, "Double type column is not inferred correctly.")
-        self.assertEquals(ts.short_col.dtype.type, np.int16, "Short type column is not inferred correctly.")
-        self.assertEquals(ts.int_col.dtype.type, np.int32, "Integer type column is not inferred correctly.")
-        self.assertEquals(ts.long_col.dtype.type, np.int64, "Long type column is not inferred correctly.")
+        self.assertEqual(ts.bool_col.dtype.type, np.bool_, "Boolean type column is not inferred correctly.")
+        self.assertEqual(ts.float_col.dtype.type, np.float32, "Float type column is not inferred correctly.")
+        self.assertEqual(ts.double_col.dtype.type, np.float64, "Double type column is not inferred correctly.")
+        self.assertEqual(ts.short_col.dtype.type, np.int16, "Short type column is not inferred correctly.")
+        self.assertEqual(ts.int_col.dtype.type, np.int32, "Integer type column is not inferred correctly.")
+        self.assertEqual(ts.long_col.dtype.type, np.int64, "Long type column is not inferred correctly.")
+        self.assertEqual(ts.str_col.dtype.type, np.object_, "String type column is not inferred correctly.")
+        self.assertEqual(ts.bin_col.dtype.type, np.object_, "Binary type column is not inferred correctly.")
 
     def test_delete(self):
         test_path = "/tmp/petastorm_test"
